7750937f097df6cb6b845af8b34cb57b328d8f1c
ihssane <anejjarihssane@gmail.com>
2024-09-30 01:10:59 +0100

    Project finished

---
+++ app/src/main/java/com/example/hellotostconstraint/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hellotostconstraint;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button count,showToast;
+    private TextView countText;
+    private int counter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        count = findViewById(R.id.count);
+        showToast = findViewById(R.id.toast);
+        countText=findViewById(R.id.countText);
+        counter=0;
+        countText.setText(String.valueOf(counter));
+        count.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View view) {
+                                         counter++;
+                                         countText.setText(String.valueOf(counter));
+
+                                     }
+                                 }
+        );
+        showToast.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_LONG).show();
+                                         }
+                                     }
+        );
+
+    }
+}
